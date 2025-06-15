91ff31e30874a5ef6d222195b7b221a3d93e3850
Manish Mannivannan <manish@hotmail.no>
2025-06-15 09:28:38 +0200

    added header to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,16 +4,19 @@ import React, { Suspense } from "react";
 import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./experience";
-import UI from "./UI"
+import UI from "./UI";
+import Header from "../components/header/header";
 
 const AboutPage: React.FC = () => {
   return (
-    <main className="relative w-full h-screen">
-      {/* Overlay UI & loading indicator */}
+    <main className="flex flex-col w-full h-screen">
+      <div className="w-full h-fit flex justify-center z-20 mt-5">
+        <Header />
+      </div>
+
       <UI />
       <Loader />
 
-      {/* 3D canvas */}
       <Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
         <group position-y={0}>
           <Suspense fallback={null}>
