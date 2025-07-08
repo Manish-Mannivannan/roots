caa97daa5c65761c946e54a7097220ae013f85f8
Sabesa1804 <97226512+Sabesa1804@users.noreply.github.com>
2025-07-08 19:13:42 +0200

    profile page init

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function ProfilePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start p-2 gap-2">
+        <h1>hello</h1>
+    </main>
+  );
+}
